8ff94401b4706cbea43a598f6345454c847efa58
BDunnCode <BDunnCode@github.com>
2023-11-09 21:31:51 -0600

    Working on the JS Event Listener Lopic

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hi');
+let frontBox = document.getElementById('frontBox')
+let toolBox = document.getElementById('')
+let backBox = document.getElementById('')
+let frontSkillBox = getElementById('frontSkills')
+let toolSkillBox = getElementById('')
+let backSkillBox = getElementById('')
+
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+  } else {
+    ready();
+  }
+
+function ready() {
+    frontBox.addEventListener("click", () => {
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// frontSkillGrabber.addEventListener('click', toggleHidden(frontSkillSection))
+
+// function toggleHidden(x) {
+//     x.toggle('hide')
+// } 
